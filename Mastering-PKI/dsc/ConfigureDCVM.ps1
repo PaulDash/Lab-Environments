2e85423dc9ddc661b15aed72c52786a79383e206
Dash.Training <48420791+PaulDash@users.noreply.github.com>
2020-03-05 02:27:44 +0100

    Add files via upload

--- Mastering-PKI/dsc/ConfigureDCVM.ps1
+++ Mastering-PKI/dsc/ConfigureDCVM.ps1
@@ -80,88 +80,13 @@
             DependsOn = "[PendingReboot]Reboot1"
         }
 
-<#
         WindowsFeature AddADFeature2    { Name = "RSAT-ADDS-Tools";     Ensure = "Present"; DependsOn = "[PendingReboot]Reboot1" }
 
-        if ($ConfigureADFS -eq $true) {
-            #**********************************************************
-            # Configure AD CS
-            #**********************************************************
-            WindowsFeature AddCertAuthority       { Name = "ADCS-Cert-Authority"; Ensure = "Present"; DependsOn = "[PendingReboot]Reboot1" }
-            WindowsFeature AddADCSManagementTools { Name = "RSAT-ADCS-Mgmt";      Ensure = "Present"; DependsOn = "[PendingReboot]Reboot1" }
-            ADCSCertificationAuthority ADCS
-            {
-                IsSingleInstance = "Yes"
-                CAType = "EnterpriseRootCA"
-                Ensure = "Present"
-                Credential = $DomainCredsNetbios
-                DependsOn = "[WindowsFeature]AddCertAuthority"
-            }
 
-            #**********************************************************
-            # Configure AD FS
-            #**********************************************************
-            WaitForCertificateServices WaitAfterADCSProvisioning
-            {
-                CAServerFQDN = "$ComputerName.$DomainFQDN"
-                CARootName = "$DomainNetbiosName-$ComputerName-CA"
-                DependsOn = '[ADCSCertificationAuthority]ADCS'
-                PsDscRunAsCredential = $DomainCredsNetbios
-            }
-
-            CertReq ADFSSiteCert
-            {
-                CARootName                = "$DomainNetbiosName-$ComputerName-CA"
-                CAServerFQDN              = "$ComputerName.$DomainFQDN"
-                Subject                   = "$ADFSSiteName.$DomainFQDN"
-                FriendlyName              = "$ADFSSiteName.$DomainFQDN site certificate"
-                KeyLength                 = '2048'
-                Exportable                = $true
-                ProviderName              = '"Microsoft RSA SChannel Cryptographic Provider"'
-                OID                       = '1.3.6.1.5.5.7.3.1'
-                KeyUsage                  = '0xa0'
-                CertificateTemplate       = 'WebServer'
-                AutoRenew                 = $true
-                SubjectAltName            = "dns=certauth.$ADFSSiteName.$DomainFQDN&dns=$ADFSSiteName.$DomainFQDN"
-                Credential                = $DomainCredsNetbios
-                DependsOn = '[WaitForCertificateServices]WaitAfterADCSProvisioning'
-            }
-
-            CertReq ADFSSigningCert
-            {
-                CARootName                = "$DomainNetbiosName-$ComputerName-CA"
-                CAServerFQDN              = "$ComputerName.$DomainFQDN"
-                Subject                   = "$ADFSSiteName.Signing"
-                FriendlyName              = "$ADFSSiteName Signing"
-                KeyLength                 = '2048'
-                Exportable                = $true
-                ProviderName              = '"Microsoft RSA SChannel Cryptographic Provider"'
-                OID                       = '1.3.6.1.5.5.7.3.1'
-                KeyUsage                  = '0xa0'
-                CertificateTemplate       = 'WebServer'
-                AutoRenew                 = $true
-                Credential                = $DomainCredsNetbios
-                DependsOn = '[WaitForCertificateServices]WaitAfterADCSProvisioning'
-            }
-
-            CertReq ADFSDecryptionCert
-            {
-                CARootName                = "$DomainNetbiosName-$ComputerName-CA"
-                CAServerFQDN              = "$ComputerName.$DomainFQDN"
-                Subject                   = "$ADFSSiteName.Decryption"
-                FriendlyName              = "$ADFSSiteName Decryption"
-                KeyLength                 = '2048'
-                Exportable                = $true
-                ProviderName              = '"Microsoft RSA SChannel Cryptographic Provider"'
-                OID                       = '1.3.6.1.5.5.7.3.1'
-                KeyUsage                  = '0xa0'
-                CertificateTemplate       = 'WebServer'
-                AutoRenew                 = $true
-                Credential                = $DomainCredsNetbios
-                DependsOn = '[WaitForCertificateServices]WaitAfterADCSProvisioning'
-            }
-
-
+# CONSIDER CREATING DNS ENTIRES FOR
+# CA-LINUX
+# CA-ROOT
+<#
             xDnsRecord AddADFSHostDNS {
                 Name = $ADFSSiteName
                 Zone = $DomainFQDN
@@ -169,38 +94,7 @@
                 Type = "ARecord"
                 Ensure = "Present"
                 DependsOn = "[PendingReboot]Reboot1"
-            }
-
-            xScript ExportCertificates
-            {
-                SetScript = 
-                {
-                    Write-Verbose -Message "Exporting the public key of the ADFS signing certifiacte and its issuer..."
-                    $ADFSSiteName = $using:ADFSSiteName
-                    $destinationPath = "C:\Setup"
-                    $adfsSigningCertFileName = "ADFS Signing.cer"
-                    $adfsSigningIssuerCertFileName = "ADFS Signing issuer.cer"
-                    New-Item $destinationPath -Type directory -ErrorAction SilentlyContinue
-
-                    $signingCert = Get-ChildItem -Path "cert:\LocalMachine\My\" -DnsName "$ADFSSiteName.Signing"
-                    $signingCert| Export-Certificate -FilePath ([System.IO.Path]::Combine($destinationPath, $adfsSigningCertFileName))
-                    Get-ChildItem -Path "cert:\LocalMachine\Root\"| Where-Object{$_.Subject -eq  $signingCert.Issuer}| Select-Object -First 1| Export-Certificate -FilePath ([System.IO.Path]::Combine($destinationPath, $adfsSigningIssuerCertFileName))
-                    Write-Verbose -Message "Public key of the ADFS signing certifiacte and its issuer successfully exported"
-                }
-                GetScript =  
-                {
-                    # This block must return a hashtable. The hashtable must only contain one key Result and the value must be of type String.
-                    return @{ "Result" = "false" }
-                }
-                TestScript = 
-                {
-                    # If it returns $false, the SetScript block will run. If it returns $true, the SetScript block will not run.
-                return $false
-                }
-                DependsOn = "[CertReq]ADFSSiteCert", "[CertReq]ADFSSigningCert", "[CertReq]ADFSDecryptionCert"
-            }
-
-        }
+            }        
 #>
     }
 }
@@ -247,10 +141,8 @@ Install-Module -Name xNetworking
 help ConfigureDCVM
 
 $Admincreds = Get-Credential -Credential "yvand"
-$AdfsSvcCreds = Get-Credential -Credential "adfssvc"
 $DomainFQDN = "contoso.local"
-$PrivateIP = "10.0.1.4"
-$ConfigureADFS = $false
+$PrivateIP = "10.20.1.20"
 
 $outputPath = "C:\Packages\Plugins\Microsoft.Powershell.DSC\2.80.0.0\DSCWork\ConfigureDCVM.0\ConfigureDCVM"
 ConfigureDCVM -Admincreds $Admincreds -AdfsSvcCreds $AdfsSvcCreds -DomainFQDN $DomainFQDN -PrivateIP $PrivateIP -ConfigureADFS $ConfigureADFS -ConfigurationData @{AllNodes=@(@{ NodeName="localhost"; PSDscAllowPlainTextPassword=$true })} -OutputPath $outputPath
---
+++ Mastering-PKI/dsc/ConfigureIISVM.ps1
@@ -0,0 +1,96 @@
+configuration ConfigureSQLVM
+{
+    param
+    (
+        [Parameter(Mandatory)] [String]$DNSServer,
+        [Parameter(Mandatory)] [String]$DomainFQDN,
+        [Parameter(Mandatory)] [System.Management.Automation.PSCredential]$DomainAdminCreds
+    )
+
+    Import-DscResource -ModuleName ComputerManagementDsc, NetworkingDsc, xActiveDirectory, xPSDesiredStateConfiguration
+
+    [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
+    $Interface = Get-NetAdapter| Where-Object Name -Like "Ethernet*"| Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+    [PSCredential] $DomainAdminCredsQualified = New-Object PSCredential ("${DomainNetbiosName}\$($DomainAdminCreds.UserName)", $DomainAdminCreds.Password)
+    $ComputerName = Get-Content env:computername
+    [Int] $RetryCount = 30
+    [Int] $RetryIntervalSec = 30
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+        #**********************************************************
+        # Initialization of VM
+        #**********************************************************
+###        WindowsFeature ADTools  { Name = "RSAT-AD-Tools";      Ensure = "Present"; }
+###        WindowsFeature ADPS     { Name = "RSAT-AD-PowerShell"; Ensure = "Present"; }
+        DnsServerAddress DnsServerAddress { Address = $DNSServer; InterfaceAlias = $InterfaceAlias; AddressFamily  = 'IPv4' }
+
+        #**********************************************************
+        # Join AD forest
+        #**********************************************************
+        xWaitForADDomain DscForestWait
+        {
+            DomainName           = $DomainFQDN
+            DomainUserCredential = $DomainAdminCredsQualified
+            RetryCount           = $RetryCount
+            RetryIntervalSec     = $RetryIntervalSec
+            #DependsOn            = "[DnsServerAddress]DnsServerAddress"
+        }
+
+        Computer DomainJoin
+        {
+            Name = $ComputerName
+            DomainName = $DomainFQDN
+            Credential = $DomainAdminCredsQualified
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+    }
+}
+
+function Get-NetBIOSName
+{
+    [OutputType([string])]
+    param(
+        [string]$DomainFQDN
+    )
+
+    if ($DomainFQDN.Contains('.')) {
+        $length=$DomainFQDN.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainFQDN.Substring(0,$length)
+    }
+    else {
+        if ($DomainFQDN.Length -gt 15) {
+            return $DomainFQDN.Substring(0,15)
+        }
+        else {
+            return $DomainFQDN
+        }
+    }
+}
+
+<#
+# Azure DSC extension logging: C:\WindowsAzure\Logs\Plugins\Microsoft.Powershell.DSC\2.21.0.0
+# Azure DSC extension configuration: C:\Packages\Plugins\Microsoft.Powershell.DSC\2.21.0.0\DSCWork
+Install-Module -Name SqlServerDsc
+
+help ConfigureSQLVM
+$DomainAdminCreds = Get-Credential -Credential "yvand"
+$SqlSvcCreds = Get-Credential -Credential "sqlsvc"
+$SPSetupCreds = Get-Credential -Credential "spsetup"
+$DNSServer = "10.0.1.4"
+$DomainFQDN = "contoso.local"
+
+ConfigureSQLVM -DNSServer $DNSServer -DomainFQDN $DomainFQDN -DomainAdminCreds $DomainAdminCreds -SqlSvcCreds $SqlSvcCreds -SPSetupCreds $SPSetupCreds -ConfigurationData @{AllNodes=@(@{ NodeName="localhost"; PSDscAllowPlainTextPassword=$true })} -OutputPath "C:\Data\\output"
+Start-DscConfiguration -Path "C:\Data\output" -Wait -Verbose -Force
+
+#>
